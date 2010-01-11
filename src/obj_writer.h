69c8d6138cee7e1d55515498513f8e01d3c0c540
Burning Daylight <dp@bestpocketgames.com>
2010-01-12 01:05:09 +0300

    Added minor comment

--- src/obj_writer.h
+++ src/obj_writer.h
@@ -3,6 +3,7 @@
 
 struct point_group3D_t;
 
+// Note: useful only for 3D point groups
 void write_group_of_blocks_info_obj_file(
 																					const char*		  			filename,
 																					const char*						header, 
